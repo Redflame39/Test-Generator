a420e12abdc6e2a6452928df886c9d3e4fc65532
Redflame39 <vanya133719@gmail.com>
2021-11-14 23:47:48 +0300

    ConstructorData class

---
+++ TestGenerator/MembersData/ConstructorData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator.MembersData
+{
+    public class ConstructorData
+    {
+        public string ReturnValueType { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Dictionary<string, string> Parameters { get; private set; }
+
+        public ConstructorData(string returnValue, string name, Dictionary<string, string> parametersMap)
+        {
+            ReturnValueType = returnValue;
+            Name = name;
+            Parameters = parametersMap;
+        }
+    }
+}
