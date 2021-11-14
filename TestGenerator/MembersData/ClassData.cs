029f155edd90e4b0cf9ffdd1eeee54dbfd7c4fb0
Redflame39 <vanya133719@gmail.com>
2021-11-14 23:59:15 +0300

    ClassData class

---
+++ TestGenerator/MembersData/ClassData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator.MembersData
+{
+    public class ClassData
+    {
+        public string Name { get; private set; }
+
+        public List<ConstructorData> Constructors { get; private set; }
+
+        public List<MethodData> Methods { get; private set; }
+
+        public ClassData(string name, List<ConstructorData> constructors, List<MethodData> methods)
+        {
+            Name = name;
+            Constructors.AddRange(constructors);
+            Methods.AddRange(methods);
+        }
+    }
+}
