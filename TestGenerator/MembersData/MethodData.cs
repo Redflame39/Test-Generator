93781efdcf27d98be1891aecbe42dd6fa8af2cf3
Redflame39 <vanya133719@gmail.com>
2021-11-14 23:45:33 +0300

    MethodData class

---
+++ TestGenerator/MembersData/MethodData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator.MembersData
+{
+    public class MethodData
+    {
+        public string Name { get; private set; }
+
+        public Dictionary<string, string> Parameters { get; private set; }
+
+        public string ReturnValueType { get; private set; }
+
+        public MethodData(string name, Dictionary<string, string> parametersMap, string returnType)
+        {
+            Name = name;
+            Parameters = parametersMap;
+            ReturnValueType = returnType;
+        }
+    }
+}
