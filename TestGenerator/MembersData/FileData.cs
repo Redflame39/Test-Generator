5ef216e32c4fae7da1bf6da646f878ceb0f3c69f
Redflame39 <vanya133719@gmail.com>
2021-11-15 00:02:34 +0300

    FileData class

---
+++ TestGenerator/MembersData/FileData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator.MembersData
+{
+    public class FileData
+    {
+        public List<ClassData> Classes { get; private set; }
+
+        public FileData(List<ClassData> classes)
+        {
+            Classes.AddRange(classes);
+        }
+    }
+}
