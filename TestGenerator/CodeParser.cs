5196f65721b834c013232c3b4c8024ea84f37698
Redflame39 <vanya133719@gmail.com>
2021-11-15 12:33:22 +0300

    CodeParser class, ConstructorData removed property ReturnType

---
+++ TestGenerator/CodeParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using TestGenerator.MembersData;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestGenerator
+{
+    public class CodeParser
+    {
+        public FileData GetFileData(string source)
+        {
+            CompilationUnitSyntax tree = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+            List<ClassData> classes = new List<ClassData>();
+            foreach (var classDeclaration in tree.DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                classes.Add(GetClassData(classDeclaration));
+            }
+
+            return new FileData(classes);
+        }
+
+        private ClassData GetClassData(ClassDeclarationSyntax syntax)
+        {
+
+            List<ConstructorData> ctors = new List<ConstructorData>();
+            foreach (var ctor in syntax.DescendantNodes()
+                .OfType<ConstructorDeclarationSyntax>()
+                .Where((cd) => cd.Modifiers
+                .Any((c) => c.IsKind(SyntaxKind.PublicKeyword))))
+            {
+                ctors.Add(GetConstructorData(ctor));
+            }
+
+            List<MethodData> methods = new List<MethodData>();
+            foreach (var method in syntax.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Where((md) => md.Modifiers
+                .Any((m) => m.IsKind(SyntaxKind.PublicKeyword))))
+            {
+                methods.Add(GetMethodData(method));
+            }
+
+            return new ClassData(syntax.Identifier.ValueText, ctors, methods);
+        }
+
+        private ConstructorData GetConstructorData(ConstructorDeclarationSyntax syntax)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            foreach (var param in syntax.ParameterList.Parameters)
+            {
+                parameters.Add(param.Identifier.Text, param.Type.ToString());
+            }
+
+            return new ConstructorData(syntax.Identifier.Text, parameters);
+        }
+
+        private MethodData GetMethodData(MethodDeclarationSyntax syntax)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            foreach (var param in syntax.ParameterList.Parameters)
+            {
+                parameters.Add(param.Identifier.Text, param.Type.ToString());
+            }
+
+            return new MethodData(syntax.Identifier.Text, parameters, syntax.ReturnType.ToString());
+        }
+    }
+}
--- TestGenerator/MembersData/ConstructorData.cs
+++ TestGenerator/MembersData/ConstructorData.cs
@@ -8,15 +8,13 @@ namespace TestGenerator.MembersData
 {
     public class ConstructorData
     {
-        public string ReturnValueType { get; private set; }
 
         public string Name { get; private set; }
 
         public Dictionary<string, string> Parameters { get; private set; }
 
-        public ConstructorData(string returnValue, string name, Dictionary<string, string> parametersMap)
+        public ConstructorData(string name, Dictionary<string, string> parametersMap)
         {
-            ReturnValueType = returnValue;
             Name = name;
             Parameters = parametersMap;
         }
